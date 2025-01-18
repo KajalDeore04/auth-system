0a22eb487678bbe15f5f7813a690c5d1c59d5628
KajalDeore04 <kajaldeore04@gmail.com>
2025-01-18 10:00:58 +0530

    bg img updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,8 @@ import Header from '../components/Header';
 
 const Home = () => {
     return (
-        <div className='flex flex-col items-center justify-center min-h-screen bg-[url("/bg_img.png")] bg-cover bg-center'>
+        <div className='flex flex-col items-center justify-center min-h-screen bg-cover bg-center'
+        style={{ backgroundImage: "url('/src/assets/bg_img.png')" }}>
             <Navbar />
             <Header />
         </div>
